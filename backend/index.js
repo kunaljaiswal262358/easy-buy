f39115770c3eb38f64960426227222f8dd032952
kunaljaiswal262358 <157969512+kunaljaiswal262358@users.noreply.github.com>
2025-03-28 12:50:19 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,5 @@ require('./startup/routes')(app)
 
 if(!config.get('jwtPrivateKey'))
     process.exit(1)
-console.log(config.get('mongoUri'))
 
 app.listen(5000, () => console.log(`Server running on ${5000}`));
