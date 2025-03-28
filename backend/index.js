0a3cc5f5ab8cb5cc980a6d6fb2caa90fd60bc15a
kunaljaiswal262358 <157969512+kunaljaiswal262358@users.noreply.github.com>
2025-03-28 12:49:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,4 +9,4 @@ if(!config.get('jwtPrivateKey'))
     process.exit(1)
 console.log(config.get('mongoUri'))
 
-app.listen(5000, () => console.log(`Server running on http://localhost:5000`));
+app.listen(5000, () => console.log(`Server running on ${5000}`));
